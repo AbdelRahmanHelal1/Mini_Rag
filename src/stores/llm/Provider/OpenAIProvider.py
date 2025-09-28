16d293ac54396ce8d2916be3e00c9b2d66f66786
AbdelRahmanHelal1 <bdwhlal47@gmail.com>
2025-09-28 18:12:08 +0300

    create openaiprovider

---
+++ src/stores/llm/Provider/OpenAIProvider.py
@@ -0,0 +1,74 @@
+from ..LLMInterface import LLMInterface
+from openai import OpenAI
+import logging
+class OpenAIProvider(LLMInterface):
+
+    def __init__(self,api_key:str,api_url:str=None,df_input_max_char:int=1000,
+                 
+                 df_output_max_char:int=1000,df_temperature:float=0.1):
+        
+        self.api_key = api_key
+        self.api_url = api_url      
+        self.df_input_max_char = df_input_max_char
+        self.df_output_max_char = df_output_max_char
+        self.df_temperature = df_temperature
+
+        self.generation_model_id=None
+        self.embedding_model_id=None
+        self.embedding_size=None
+
+        self.client=OpenAI(api_key=self.api_key,
+                           api_url=self.api_url)
+        
+        self.logger = logging.getLogger(__name__)
+
+        def set_generation_model(self, model_id: str):
+            self.generation_model_id=model_id
+
+        def set_embedding_model(self, model_id: str,embedding_size:int=None):
+            self.embedding_model_id=model_id
+            self.embedding_size=embedding_size
+
+        def generation_text(self, prompt: str,chat_history:list=[],
+                        max_out_tokecn:int=None ,
+                         temperature :float=None):
+            if not self.client:
+                raise logging.error("OpenAI client is not initialized.")
+                return None
+            
+            if not self.generation_model_id :
+                raise logging.error("Generation model is not set.")
+                return None
+            max_out_tokecn=max_out_tokecn if max_out_tokecn else self.df_output_max_char
+            temperature=temperature if temperature else self.self.df_temperature 
+            
+
+
+        def embedding_text(self, text: str, document_type: str):
+            
+            if not self.client:
+                raise logging.error("OpenAI client is not initialized.")
+                return None
+            
+            if not self.embedding_model_id :
+                raise logging.error("Embedding model is not set.")
+                return None
+            response=self.client.embeddings.create(
+                model=self.embedding_model_id,
+                input=text
+            )
+
+            if not response or not response.data or len(response.data)==0 or not  response.data[0].embedding :
+                self.logger.error("No embedding data received from OpenAI.")
+                return None
+            
+            return response.data[0].embedding
+
+            
+            
+
+        def construct_prompt(self, prompt: str, role: str):
+            return{
+                "role":role,
+                "content":prompt
+            }
